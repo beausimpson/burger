8b01de8ff66e8a1aa0afcabee3d847247c72a046
beausimpson <simpson.beau@gmail.com>
2019-01-07 17:03:01 -0600

    set up server.js, db, connection.js (need to add heroku deployment) & working on orm.js

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name) VALUES ("Green Chile Cheeseburger");
+
+INSERT INTO burgers (burger_name) VALUES ("BBQ Bacon Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Portobello Mushroom Burger");
--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 
 // dependencies
 var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// static content
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// handlebars.
+var exphbs = require("express-handlebars");
+
+// inports routes
+var routes = require("./controllers/catsController.js");
+
+app.use(routes);
+
+// starts server
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
