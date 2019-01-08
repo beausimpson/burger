4e80a53545177eb139f1f9c6d6a24f5086797452
beausimpson <simpson.beau@gmail.com>
2019-01-07 21:48:41 -0600

    working on orm.js, moved css to assets folder

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,12 @@ var express = require("express");
 var burger = require("../models/burger.js");
 
 var router = express.Router();
+
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+
+    })
+});
+
+// Export routes for server.js to use.
+module.exports = router;
